d071b6164c8131cfaff3622548015d7b17c298ea
Martin Gagné <martin_gagne@outlook.com>
2018-05-05 03:28:15 -0400

    Crawl multiple recipes

--- app/Http/Crawler/robot.py
+++ app/Http/Crawler/robot.py
@@ -5,21 +5,92 @@ from slugify import slugify
 import functions
 import json
 import isodate
+import sys
 from datetime import timedelta
 
-def recipes_spider(crawling, max_pages = 1, max_cat = 1):
-	# This is the crawling code of Ricardo's website.
-	if crawling is 'ricardo':
-		cat = 1 # Max number of categories to crawl.
-		url = 'https://www.ricardocuisine.com/en/recipes/7781-family-style-bibimbap' # URL of all the recipes categories.
+# def get_urls(url, tags):
+# 	urls = set()
+# 	source_code = requests.get(url)
+# 	plain_text = source_code.text
+# 	soup = BeautifulSoup(plain_text, 'html.parser')
+
+# 	elements = soup.findAll('ul', {'class': 'item-list'})
+
+# 	for tag in tags:
+# 		elements = soup.findAll(tag['name'], {tag['selectorType']: tag['selectorName']})
+# 		for element in elements
+# 		soup = elements
+
+# 	print(soup)
+# 	sys.exit()
+
+# 	for el2 in el1('h2'):
+# 		for el3 in el2('a'):
+# 			linkHref = link.get('href')
+# 			urls.add(linkHref)
+
+# 	return urls
+
+def get_category_urls():
+	urls = set()
+	url = 'https://www.ricardocuisine.com/en/recipes/main-dishes/'
+	source_code = requests.get(url)
+	plain_text = source_code.text
+	soup = BeautifulSoup(plain_text, 'html.parser')
+
+	categoriesWrap = soup.find('ul', {'class': 'item-list'})
+
+	for categoryH2 in categoriesWrap('h2'):
+		for link in categoryH2('a'):
+			linkHref = link.get('href')
+			urls.add(linkHref)
+
+	return list(urls)
+
+def get_recipe_urls(category_url, max_pages):
+	urls = set()
+	for page in range(1, max_pages + 1):
+		url = 'https://www.ricardocuisine.com/' + category_url + '/page/' + str(page)
+		source_code = requests.get(url)
+
+		if source_code.status_code is not 206:
+			plain_text = source_code.text
+			soup = BeautifulSoup(plain_text, 'html.parser')
+
+			recipesWrap = soup.find('ul', {'class': 'item-list'})
+			if recipesWrap is not None:
+				for recipeH2 in recipesWrap('h2'):
+					for link in recipeH2('a'):
+						linkHref = link.get('href')
+						urls.add(linkHref)
+			else:
+				break
+		else:
+			if page <= 1:
+				urls = None
+			break
+
+	return list(urls)
+
+def get_recipes(urls):
+	recipes = list()
+	for url in urls:
+		url = 'https://www.ricardocuisine.com/' + url # URL of all the recipes categories.
+		#print(url)
 		source_code = requests.get(url)
 		plain_text = source_code.text
 		soup = BeautifulSoup(plain_text, 'html.parser')
 		ingredientsWrap = soup.find('form', {'id': 'formIngredients'})
 		itemList = soup.find('script', {'type': 'application/ld+json'})
 		structuredData = json.loads(itemList.text)
-		totalPrep = isodate.parse_duration(structuredData['prepTime']).total_seconds()
-		totalCook = isodate.parse_duration(structuredData['cookTime']).total_seconds()
+
+		if structuredData['prepTime'] is not None:
+			totalPrep = isodate.parse_duration(structuredData['prepTime']).total_seconds()
+		else:
+			totalPrep = 0
+		if structuredData['cookTime'] is not None:
+			totalCook = isodate.parse_duration(structuredData['cookTime']).total_seconds()
+		else: totalCook = 0
 
 		data = {
 			'name': structuredData['name'],
@@ -53,7 +124,7 @@ def recipes_spider(crawling, max_pages = 1, max_cat = 1):
 
 			for ingredientString in ingredientsWrap.findAll(text=re.compile(ingredient, flags=re.IGNORECASE)):
 				# print(ingredientString)
-				regex = "(\([\w\s ]+\)|[0-9]+ tbsp|[0-9]+ tsp|[0-9]+ slices?|[0-9]+ sprigs?|[0-9]+ " + ingredient + ")"
+				regex = "(\([\w\s ]+\)|[0-9]+ tbsp|[0-9]+ tsp|[0-9]+ " + ingredient + ")"
 				quantity = re.search(regex, ingredientString, flags=re.IGNORECASE)
 				#print(quantity)
 				if quantity:
@@ -61,8 +132,15 @@ def recipes_spider(crawling, max_pages = 1, max_cat = 1):
 					quantity = quantity.strip('(').strip(')')
 					quantity = re.split('([0-9]+)', quantity)
 
-					volume = int(quantity[1])
-					unit = quantity[2].strip().lower()
+
+					if len(quantity) >= 2:
+						volume = int(quantity[1])
+					else:
+						volume = None
+
+					if len(quantity) >= 3:
+						unit = quantity[2].strip().lower()
+					else: unit = None
 
 					if unit is ingredient:
 						unit = 'whole'
@@ -89,6 +167,24 @@ def recipes_spider(crawling, max_pages = 1, max_cat = 1):
 				i = i + 1
 
 		data['instructions'] = '||'.join(data['instructions'])
+		recipes.append(data)
+	return recipes
+
+def recipes_spider(crawling, max_pages = 1, max_cat = 1):
+	# This is the crawling code of Ricardo's website.
+	if crawling is 'ricardo':
+		category_urls = get_category_urls()
+		recipe_urls = list()
+
+		for x in range(0, max_cat):
+			recipe_urls = recipe_urls + get_recipe_urls(category_urls[x], max_pages)
+
+		recipes = get_recipes(recipe_urls)
+		return recipes
+		sys.exit()
+
+		cat = 1 # Max number of categories to crawl.
+		
 
 		return data
 		# cat = 1 # Max number of categories to crawl.
@@ -156,7 +252,7 @@ def recipes_spider(crawling, max_pages = 1, max_cat = 1):
 					ingredient = link.text.strip().lower()
 					if 'related' not in ingredient:
 						data += [(ingredient, slugify(ingredient))]
-						print(ingredient)
+						#print(ingredient)
 
 				page += 1
 			if page > max_cat:
